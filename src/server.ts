2b1f6eaa30b8c014a30ea9387d27f0a19c86e44e
Mehedi Mehad <mdmehedihasanmehad@gmail.com>
2026-02-15 00:07:28 +0600

    feat(server): add graceful shutdown and global error handlers

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,51 @@
-import { Server } from 'http';
+import type { Server as HttpServer } from 'http';
 import app from './app';
 import config from './config';
 
-const port = config.port as string;
+
+let server: HttpServer;
 
 async function main() {
-    const server: Server = app.listen(port, () => {
-        console.log(`🚀 Server listening at http://localhost:${port} 😎`);
-    });
+    try {
+        // 🟢 Start the server
+        const port = config.port! as string;
+        server = app.listen(port, async () => {
+            console.log(`🚀 Server is running on port ${port}`);
+        });
+
+        // 🔐 Handle Uncaught Exceptions
+        process.on('uncaughtException', (error) => {
+            console.error('❌ Uncaught Exception:', error);
+            shutdown();
+        });
+
+        // 🔐 Handle Unhandled Promise Rejections
+        process.on('unhandledRejection', (reason) => {
+            console.error('❌ Unhandled Rejection:', reason);
+            shutdown();
+        });
+
+        // 🛑 Graceful Shutdown
+        process.on('SIGTERM', () => {
+            console.info('🔁 SIGTERM received.');
+            shutdown();
+        });
+    } catch (error) {
+        console.error('❌ Failed to start server:', error);
+        process.exit(1);
+    }
 }
 
-main().catch((error) => {
-    console.error('❌ Server failed to start', error);
-    process.exit(1);
-});
+// 🔁 Graceful Server Shutdown
+function shutdown() {
+    if (server) {
+        server.close(() => {
+            console.info('🔒 Server closed gracefully.');
+            process.exit(1);
+        });
+    } else {
+        process.exit(1);
+    }
+}
+
+main();
